c40bb0247dd418cb3605adf28c68bba434cd64e4
Inder Rana <inder.x.rana@gmail.com>
2023-01-19 15:28:34 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleDarkMode() {
+    var body = document.getElementsByTagName("BODY")[0];
+    var currentClass = body.className;
+    if (currentClass === "dark-mode") {
+      body.className = "light-mode";
+    } else {
+      body.className = "dark-mode";
+    }
+  }
